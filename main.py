9f9b8c9f3ef6068db64ac4e4095b915d559f9fa4
Matheus Eger da Silva <matheus.eger@hotmail.com>
2026-09-17 20:41:23 -0300

    Primeiros Passos

---
+++ main.py
@@ -0,0 +1,46 @@
+def consultar_produtos():
+    pass
+
+
+def cadastrar_produtos():
+    pass
+
+
+def apagar_produto():
+    pass
+
+
+def editar_produto():
+    pass
+
+
+def limpar_terminal():
+    import os
+    os.system("cls")
+
+if __name__ == "__main__":
+    menu = """MENU:
+1   - Consultar produtos
+2   - Cadastrar Produtos
+3   - Apagar Produto
+4   - Editar produto
+99  - Sair
+
+Digite o menu desejado: """
+
+    menu_escolhido = int(input(menu))
+
+    while menu_escolhido != 99:
+        limpar_terminal()
+        if menu_escolhido == 1:
+            consultar_produtos()
+        elif menu_escolhido == 2:
+            cadastrar_produtos()
+        elif menu_escolhido == 3:
+            apagar_produto()
+        elif menu_escolhido == 4:
+            editar_produto()
+        else:
+            print("Opção Inválida")
+
+        menu_escolhido = int(input(menu))
